326d59fb62497f3f271c629c2701346c1c79747a
TylerMG2 <tylermg17@gmail.com>
2025-07-13 15:02:28 +1000

    Created flake.nix for development

---
+++ flake.nix
@@ -0,0 +1,99 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    utils,
+    rust-overlay,
+  }:
+    utils.lib.eachDefaultSystem (
+      system: let
+        buildTarget = "wasm32-unknown-unknown";
+
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [rust-overlay.overlays.default];
+        };
+
+        rustToolchain = pkgs.rust-bin.stable.latest.default.override {
+          targets = [buildTarget];
+          extensions = ["rust-src"];
+        };
+
+        rustPlatform = pkgs.makeRustPlatform {
+          cargo = rustToolchain;
+          rustc = rustToolchain;
+        };
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            rustToolchain
+            pkgs.pkg-config
+            pkgs.openssl
+            pkgs.cargo-watch
+            pkgs.wasm-pack
+            pkgs.wasm-bindgen-cli
+            pkgs.alsa-lib
+            pkgs.rust-analyzer
+            pkgs.udev
+
+            # Add wasm-server-runner from source
+            (rustPlatform.buildRustPackage {
+              pname = "wasm-server-runner";
+              version = "1.0.0";
+              src = pkgs.fetchFromGitHub {
+                owner = "jakobhellermann";
+                repo = "wasm-server-runner";
+                rev = "v1.0.0";
+                sha256 = "sha256-3ARVVA+W9IS+kpV8j5lL/z6/ZImDaA+m0iEEQ2mSiTw=";
+              };
+              cargoHash = "sha256-FrnCmfSRAePZuWLC1/iRJ87CwLtgWRpbk6nJLyQQIT8=";
+            })
+
+            # Matchbox server for wasm
+            (rustPlatform.buildRustPackage {
+              pname = "matchbox-server";
+              version = "0.12.0";
+              src = pkgs.fetchFromGitHub {
+                owner = "johanhelsing";
+                repo = "matchbox";
+                rev = "v0.12.0";
+                sha256 = "sha256-zyA7qkBFuIEL+jxnfRzt82kEl/P5xIRySA3V0Qv2cN0=";
+              };
+              cargoBuildFlags = ["-p" "matchbox_server"];
+              doCheck = false;
+              cargoHash = "sha256-sbcZWLEgRga4iYiB2lBlZsztNjQzWWEIJHfzHovw3wU=";
+            })
+
+            # Bevy CLI
+            (rustPlatform.buildRustPackage {
+              pname = "bevy-cli";
+              version = "unstable";
+
+              nativeBuildInputs = [pkgs.pkg-config pkgs.lld];
+              buildInputs = [pkgs.openssl];
+
+              src = pkgs.fetchFromGitHub {
+                owner = "TheBevyFlock";
+                repo = "bevy_cli";
+                rev = "cli-v0.1.0-alpha.1";
+                sha256 = "sha256-v7BcmrG3/Ep+W5GkyKRD1kJ1nUxpxYlGGW3SNKh0U+8=";
+              };
+
+              cargoBuildFlags = ["--locked"];
+              cargoHash = "sha256-QrW0daIjuFQ6Khl+3sTKM0FPGz6lMiRXw0RKXGZIHC0=";
+              doCheck = false; # No network access in nix builds
+            })
+          ];
+          shellHook = ''
+            echo "Welcome to the doomsumo development shell"
+          '';
+        };
+      }
+    );
+}
